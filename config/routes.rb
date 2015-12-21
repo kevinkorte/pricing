e2394eb72b7244a81f34b2800f08bf82812f32e8
Kevin Korte <kevinkorte15@gmail.com>
2015-12-21 16:53:06 +0000

    set up auth and unauth root routes with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   #get 'welcome/index'
+  
+  unauthenticated :user do
+    devise_scope :user do
+      root 'devise/sessions#new'
+    end
+  end
+  
+  authenticated :user do
+    root 'welcome#index', as: :authentiated_root
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
